c4dad21c9076b8b0256e3a4abfd72eb801770f82
djovanovic <skr.dejanjovanovic@gmail.com>
2013-11-11 18:01:42 +0100

    MyArrayAdapter implementation

--- gen/com/infobip/campus/chat2push/android/R.java
+++ gen/com/infobip/campus/chat2push/android/R.java
@@ -1104,13 +1104,14 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int list_channel_item=0x7f030018;
-        public static final int login_layout=0x7f030019;
+        public static final int activity_channel_list=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int list_channel_item=0x7f030019;
         public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int channel_list=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1156,6 +1157,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000f;
+        public static final int title_activity_channel_list=0x7f0a0010;
     }
     public static final class style {
         /** 
---
+++ src/com/infobip/campus/chat2push/android/ChannelListActivity.java
@@ -0,0 +1,25 @@
+package com.infobip.campus.chat2push.android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+
+public class ChannelListActivity extends ActionBarActivity {
+	
+	MyArrayAdapter listViewAdapter = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_channel_list);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.channel_list, menu);
+		return true;
+	}
+
+}
