0d3f5dea752941bb66f5afed5865f80e976f54d8
unknown <jantolis@IB-JANTOLIS.infobip.local>
2013-11-11 11:13:44 +0100

    Initial commit

---
+++ gen/com/infobip/campus/chat2push/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.infobip.campus.chat2push.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
