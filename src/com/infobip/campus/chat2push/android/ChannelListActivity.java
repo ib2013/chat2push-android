4e3e2c1015a2b86b919fed7a7f0a03f23d22f8c7
djovanovic <skr.dejanjovanovic@gmail.com>
2013-11-12 11:13:52 +0100

    ChannelListActivity changes

--- src/com/infobip/campus/chat2push/android/ChannelListActivity.java
+++ src/com/infobip/campus/chat2push/android/ChannelListActivity.java
@@ -1,20 +1,41 @@
 package com.infobip.campus.chat2push.android;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.infobip.campus.chat2push.android.adapters.ChannelArrayAdapter;
+import com.infobip.campus.chat2push.android.client.DefaultInfobipClient;
+import com.infobip.campus.chat2push.android.configuration.Configuration;
 import com.infobip.campus.chat2push.android.models.ChannelModel;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.FeatureInfo;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 public class ChannelListActivity extends ActionBarActivity {
 	
 	ChannelArrayAdapter listViewAdapter = null;
-	final ArrayList<ChannelModel> channelList = new ArrayList<ChannelModel>();
+	ArrayList<ChannelModel> channelList;
 	ChannelModel mdl1 = new ChannelModel();
 	ChannelModel mdl2 = new ChannelModel();
 	
@@ -23,16 +44,19 @@ public class ChannelListActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_channel_list);
-		mdl1.setName("Ime kanala 1");
-		mdl1.setStatus(true);
-		mdl1.setDescription("Description kanala 1");
 		
-		mdl2.setName("Ime kanala 2");
-		mdl2.setStatus(true);
-		mdl2.setDescription("Description kanala 2");
+		new LoadAllChannels().execute();
 		
-		channelList.add(mdl1);
-		channelList.add(mdl2);
+//		mdl1.setName("Ime kanala 1");
+//		mdl1.setStatus(false);
+//		mdl1.setDescription("Description kanala 1");
+//		
+//		mdl2.setName("Ime kanala 2");
+//		mdl2.setStatus(true);
+//		mdl2.setDescription("Description kanala 2");
+//		
+//		channelList.add(mdl1);
+//		channelList.add(mdl2);
 		
 		displayListView(channelList);
 		
@@ -50,8 +74,95 @@ public class ChannelListActivity extends ActionBarActivity {
 		// kreiraj ArrayAdaptar iz String Array		
 		listViewAdapter = new ChannelArrayAdapter(this, R.layout.activity_channel_list, channelList);
 		ListView listView = (ListView) findViewById(R.id.listView);
-		// dodeli adapter u ListView
 		listView.setAdapter(listViewAdapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				if(arg2==1) {
+					Intent intent = new Intent(ChannelListActivity.this, ChannelActivity.class);
+					startActivity(intent);
+				}
+				
+				
+			}
+		});
+		// dodeli adapter u ListView
+		
+	}
+	
+	class LoadAllChannels extends AsyncTask<String, String, String> {
+		int errorCode = 0;
+		String textView = "";
+
+		// metoda koja postavi ProgressDialog
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+//			pDialog = new ProgressDialog(MainActivity.this);
+//			pDialog.setMessage("Loading channels. Please wait....");
+//			pDialog.setIndeterminate(false);
+//			pDialog.setCancelable(false);
+//			pDialog.show();
+		}
+
+		// run metoda zahteva
+		protected String doInBackground(String... args) {
+			// UCITAVANJE KANALA
+			// httpGet request sa odgovarajuceg urla
+			try {
+				
+				channelList = DefaultInfobipClient.fetchAllChannels(Configuration.CURRENT_USER_NAME);
+//				HttpClient client = new DefaultHttpClient();
+//				HttpGet request = new HttpGet(Conf.url_all_channels);
+//				request.addHeader(Conf.headerName, Conf.headerValue);
+//				HttpResponse response = client.execute(request);
+//				// ucitavanje tog dobijenog odgovora u string format u JSON
+//				// obliku
+//				BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+//				String line = "";
+//				while ((line = rd.readLine()) != null) {
+//					textView += line;
+//				}
+//				
+//				// parsiranje JSON formata u format ArrayList<String> channels
+//				try {
+//					JSONArray oneArray = new JSONArray(textView);
+//					for (int i = 0; i < oneArray.length(); i++) {
+//						JSONObject c = oneArray.getJSONObject(i);
+//						String name = c.getString("name");
+//						channels.add(name);
+//					}
+//				} catch (JSONException e) {
+//					e.printStackTrace();
+//					errorCode = 1;
+//				}
+			} catch (Exception e) {
+				Log.d("ERROR LOADING CHANNELS: ", "FETCHING PROBLEM");
+				errorCode = 1;
+				e.printStackTrace();
+			}
+			return "prazan string";
+		}
+
+		// metoda koja ukloni ProgressDialog i osvezi UI. Osvezavanje UIa samo u
+		// PostExecute metodi. Nikako u doInBackground()
+		protected void onPostExecute(String file_url) {
+//			pDialog.dismiss();
+			runOnUiThread(new Runnable() {
+				public void run() {
+//					if (errorCode == 0) {
+//						addItemsOnListView();
+//						writeChannelListToFile(channels);
+//					} else {
+//						Toast.makeText(MainActivity.this, "ERROR - CONNECTION PROBLEM", Toast.LENGTH_LONG).show();
+//					}
+				}
+			});
+		}
+
 	}
 
 }
