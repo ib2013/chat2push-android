a2cbac395f2d48bdc0b0ecea25e2b39cf559cbf2
Josip <jantolis@IB-JANTOLIS.infobip.local>
2013-11-12 20:34:48 +0100

    primanje notifikacija prije mergea

--- src/com/infobip/campus/chat2push/android/ChannelActivity.java
+++ src/com/infobip/campus/chat2push/android/ChannelActivity.java
@@ -2,18 +2,29 @@ package com.infobip.campus.chat2push.android;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import com.infobip.campus.chat2push.android.MyPushReceiver.CallbackInterface;
 import com.infobip.campus.chat2push.android.adapters.MessageArrayAdapter;
+import com.infobip.campus.chat2push.android.adapters.MyApplication;
+import com.infobip.campus.chat2push.android.configuration.Configuration;
 import com.infobip.campus.chat2push.android.models.MessageModel;
+import com.infobip.push.ChannelRegistrationListener;
+import com.infobip.push.PushNotificationBuilder;
+import com.infobip.push.PushNotificationManager;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class ChannelActivity extends Activity {
-
+public class ChannelActivity extends Activity implements CallbackInterface {
+	
+	protected MyApplication myApplication;
+	protected String channelName = "";
 	MessageArrayAdapter messageViewAdapter = null;
 	final ArrayList<MessageModel> messageList = new ArrayList<MessageModel>();
 	
@@ -23,6 +34,13 @@ public class ChannelActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_channel);
 		
+		myApplication = (MyApplication)this.getApplicationContext();
+		
+		Intent intent = getIntent();
+		channelName = intent.getStringExtra("channelName") + "TEST";
+		this.setTitle(channelName);
+		
+		//Hardcodeano za testiranje:
 		messageList.add(new MessageModel("Ljudina", "Maaajke mi; ja sam ljud'na! A buduæi da bi chat valjalo testirati na malo veèim stringovima još æu par stvari napisati!", new Date(System.currentTimeMillis())));
 		messageList.add(new MessageModel("Druga Ljudina", "Maaajke mi; i ja sam!!! I još bih samo nadodao da ova Ljudina iznad mene nije ništa veèa ljudima OD mene, pa æu i ja još nešto napisati. I to nešto æe biti veèe od ovoga iznad. Nek se zna.", new Date(System.currentTimeMillis())));
 		messageList.add(new MessageModel("3. Ljudina", "Da.", new Date(System.currentTimeMillis())));
@@ -30,6 +48,20 @@ public class ChannelActivity extends Activity {
 		messageList.add(new MessageModel("Druga Ljudina", "Maaajke mi; i ja sam!!! I još bih samo nadodao da ova Ljudina iznad mene nije ništa veèa ljudima OD mene, pa æu i ja još nešto napisati. I to nešto æe biti veèe od ovoga iznad. Nek se zna.", new Date(System.currentTimeMillis())));
 		messageList.add(new MessageModel("3. Ljudina", "Da.", new Date(System.currentTimeMillis())));
 		displayListView(messageList);
+		
+		//PRIVREMENA!!! prijava na infobipov server za testiranje notifikacija!
+		PushNotificationManager manager;
+		PushNotificationBuilder builder;
+		
+		manager = new PushNotificationManager(getApplicationContext());
+		manager.initialize(Configuration.SENDER_ID, Configuration.APP_ID, Configuration.APP_SECRET);
+		if (!manager.isRegistered()) 
+			manager.register();
+		manager.overrideDefaultMessageHandling(true);	
+		ChannelRegistrationListener channelRegistrationListener = null;
+		List<String> channels = new ArrayList<String>();
+		channels.add("TEST");
+		manager.registerToChannels(channels, true, channelRegistrationListener);
 		
 	}
 
@@ -39,6 +71,35 @@ public class ChannelActivity extends Activity {
 		getMenuInflater().inflate(R.menu.channel, menu);
 		return true;
 	}
+	
+	@Override
+	protected void onResume() {
+        super.onResume();
+        myApplication.setCurrentActivity(this);
+    }
+	
+	@Override
+    protected void onPause() {
+        clearReferences();
+        super.onPause();
+    }
+	
+	@Override
+    protected void onDestroy() {        
+        clearReferences();
+        super.onDestroy();
+    }
+	
+	public void addNewMessage (MessageModel newMessage) {
+		messageList.add(newMessage);
+		displayListView(messageList);
+	}
+	
+	private void clearReferences(){
+        Activity currActivity = myApplication.getCurrentActivity();
+        if (currActivity != null && currActivity.equals(this))
+        	myApplication.setCurrentActivity(null);
+    }
 	
 	private void displayListView(ArrayList<MessageModel> messageList) {
 		// kreiraj ArrayAdaptar iz String Array		
